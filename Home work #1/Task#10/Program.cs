16fbaf92a2951090df08671cd315c358ed56ab39
Fabr <fabrics.andrey@yandex.ru>
2023-10-30 18:41:39 +0300

    new HW

---
+++ Home work #1/Task#10/Program.cs
@@ -0,0 +1,9 @@
+﻿
+    Console.Write("Введи трёхзначное число: ");
+    int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+    if (threeDigitNumber < 0 || threeDigitNumber > 100)
+    {
+    string stringNumber = Convert.ToString(threeDigitNumber);
+    Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+    }
+    else Console.WriteLine("Вы ввели не 3-x значное число");
---
+++ Home work #1/Task№15/Program.cs
@@ -0,0 +1,30 @@
+﻿    
+    Console.Write("Введите номер дня недели: ");
+    int  NumOfDay = Convert.ToInt32(Console.ReadLine());
+    switch (NumOfDay)
+    {
+    case 1:
+        Console.WriteLine($"{NumOfDay} -> нет");
+        break;
+     case 2:
+        Console.WriteLine($"{NumOfDay} -> нет");
+        break;
+     case 3:
+        Console.WriteLine($"{NumOfDay} -> нет");
+        break;
+     case 4:
+        Console.WriteLine($"{NumOfDay} -> нет");
+        break;
+     case 5:
+        Console.WriteLine($"{NumOfDay} -> нет");
+        break;
+     case 6:
+        Console.WriteLine($"{NumOfDay} -> да");
+        break;
+     case 7:
+        Console.WriteLine($"{NumOfDay} -> да");
+        break;
+     default:
+        Console.WriteLine("В неделе только 7 дней)");
+        break;
+    }
